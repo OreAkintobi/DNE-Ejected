0aeff57978eb4d45dd098d29be7d6e6151410d94
OreAkintobi <oreakintobi@gmail.com>
2020-08-05 11:12:28 +0100

    reduce settimeout time for splash screen

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default function App() {
     setTimeout(async () => {
       await SplashScreen.hideAsync();
       setIsReady(true);
-    }, 2000);
+    }, 1000);
   }, [isReady]);
 
   if (!isReady) return null;
